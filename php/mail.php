7b9f6f1ca1bb9c6fba9ae8fa34d7b411c8f4a753
Timothe Medico <medico.timothe@gmail.com>
2018-05-09 14:28:41 +0200

    create mail system

--- php/mail.php
+++ php/mail.php
@@ -1,57 +1,45 @@
 <?php
-    $name = $_POST["name"];
+    /*$name = $_POST["name"];
     $mail = $_POST["mail"];
     $subject = $_POST["subject"];
-    $message = $_POST["message"];
+    $message = $_POST["message"];*/
 
-    if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)){ // On filtre les serveurs qui rencontrent des bogues.
-        $passage_ligne = "\r\n";
-    } else {
-        $passage_ligne = "\n";
-    }
+    // Plusieurs destinataires
+    $to  = 'medico.timothe@gmail.com'; // notez la virgule
 
-    //=====Déclaration des messages au format texte et au format HTML.
-    $message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
-    $message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
-    //==========
+    // Sujet
+    $subject = "Nouveau message depuis le site";
 
-    //=====Création de la boundary
-    $boundary = "-----=".md5(rand());
-    //==========
+    // message
+    $message = "<h2>Ceci est un message de test</h2><br/><br/><p>Veuillez ne pas y répondre</p>";
 
-    //=====Création du header de l'e-mail.
-    $header = "From: \"le Houx-Vert\"<contact@gitecolo.fr>".$passage_ligne;
-    $header.= "Reply-to: \"". $name ."\"<". $mail .">".$passage_ligne;
-    $header.= "MIME-Version: 1.0".$passage_ligne;
-    $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-    //==========
+    // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+    $headers['MIME-Version'] = '1.0';
+    $headers['Content-type'] = 'text/html; charset=iso-8859-1';
 
-    //=====Création du message.
-    $message = $passage_ligne."--".$boundary.$passage_ligne;
-    //=====Ajout du message au format texte.
-    $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-    $message.= $passage_ligne.$message_txt.$passage_ligne;
-    //==========
-    $message.= $passage_ligne."--".$boundary.$passage_ligne;
-    //=====Ajout du message au format HTML
-    $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-    $message.= $passage_ligne.$message_html.$passage_ligne;
-    //==========
-    $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-    $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-    //==========
+    // En-têtes additionnels
+    $headers['To'] = 'Timothé Medico <medico.timothe@gmail.com>';
+    $headers['From'] = 'le Houx-Vert <contact@gitecolo.fr>';
 
-    //=====Envoi de l'e-mail.
-    mail($mail,$sujet,$message,$header);
-    //==========
-
-
-    echo "Vous allez être redirigé vers la page principale, <a href=\"../index.php\"> cliquer ici </a> si rien ne se passe";
-    sleep(5);
-
-
-    header("Location: http://www.baragouin.fr");
-    die();
+    // Envoi
+    mail($to, $subject, $message, $headers);
 ?>
+
+<html>
+<head>
+    <title>le Houx Vert - Gîte écolo</title>
+    <link rel="icon" type="image/png" href="../favicon.png"/>
+</head>
+<body>
+    <p>Vous allez être redirigé vers la page principale, <a href="../index.php"> cliquer ici </a> si rien ne se passe</p>
+
+
+    <?php
+        sleep(5);
+
+
+        header("Location: http://www.baragouin.fr");
+        die();
+    ?>
+</body>
+</html>
