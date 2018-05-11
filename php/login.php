d00dea4611e0c87d46bd8f006d2dcbe0f23e9a53
Timothe Medico <medico.timothe@gmail.com>
2018-05-11 16:30:41 +0200

    update login system

--- php/login.php
+++ php/login.php
@@ -6,7 +6,7 @@
 <body>
     <div id="main_wrapper">
         <div id="login-container">
-            <form id="login" action="login.php" method="post">
+            <form id="login" action="login_secure.php" method="post">
                 <div class="title">
                     Identification requise
                 </div>
@@ -29,30 +29,3 @@
     </div>
 </body>
 </html>
-
-<?php
-    echo "Here";
-    echo "Username: " . $username;
-    echo "Password: " . $password;
-
-    if(isset($_POST["username"]) && isset($_POST["password"])){
-        echo "1";
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        try {
-            $bdd = new PDO("mysql:host=localhost;dbname=bruno;charset=utf8", "root", "Hra8rCJz", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        } catch (Exception $e){
-            die("Erreur : " . $e -> getMessage());
-        }
-
-        $reply = $bdd -> query("SELECT * FROM admin WHERE username = " . $username . " AND password = " . $password);
-
-        if($result < 0){
-            echo "Nom d'utilisateur ou mot de passe éronné";
-        } else {
-            echo "Bienvenue " . $username;
-        }
-    }
-
-?>
---
+++ php/login_secure.php
@@ -0,0 +1,26 @@
+<?php
+echo "Here";
+echo "Username: " . $_POST["username"];
+echo "Password: " . $_POST["password"];
+
+if(isset($_POST["username"]) && isset($_POST["password"])){
+    echo "1";
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    try {
+        $bdd = new PDO("mysql:host=localhost;dbname=bruno;charset=utf8", "root", "Hra8rCJz", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (Exception $e){
+        die("Erreur : " . $e -> getMessage());
+    }
+
+    $reply = $bdd -> query("SELECT * FROM admin WHERE username = " . $username . " AND password = " . $password);
+
+    if($result < 0){
+        echo "Nom d'utilisateur ou mot de passe éronné";
+    } else {
+        echo "Bienvenue " . $username;
+    }
+}
+
+?>
