e65db4982f7c24d68e7aabf1b0eee2cf7c1ea352
Timothe Medico <medico.timothe@gmail.com>
2018-05-11 16:34:30 +0200

    update login system

--- php/login_secure.php
+++ php/login_secure.php
@@ -1,24 +1,27 @@
 <?php
-echo "Username: " . $_POST["username"] . "<br>";
-echo "Password: " . $_POST["password"] . "<br>";
+    echo "Username: " . $_POST["username"] . "<br>";
+    echo "Password: " . $_POST["password"] . "<br>";
 
-if(isset($_POST["username"]) && isset($_POST["password"])){
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    if(isset($_POST["username"]) && isset($_POST["password"])){
+        echo "Variables set";
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
-    try {
-        $bdd = new PDO("mysql:host=localhost;dbname=bruno;charset=utf8", "root", "Hra8rCJz", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    } catch (Exception $e){
-        die("Erreur : " . $e -> getMessage());
+        try {
+            $bdd = new PDO("mysql:host=localhost;dbname=bruno;charset=utf8", "root", "Hra8rCJz", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        } catch (Exception $e){
+            die("Erreur : " . $e -> getMessage());
+        }
+
+        echo "Database connected";
+
+        $reply = $bdd -> query("SELECT * FROM admin WHERE username = " . $username . " AND password = " . $password);
+
+        if($reply < 0){
+            echo "Nom d'utilisateur ou mot de passe éronné";
+        } else {
+            echo "Bienvenue " . $username;
+        }
     }
-
-    $reply = $bdd -> query("SELECT * FROM admin WHERE username = " . $username . " AND password = " . $password);
-
-    if($reply < 0){
-        echo "Nom d'utilisateur ou mot de passe éronné";
-    } else {
-        echo "Bienvenue " . $username;
-    }
-}
 
 ?>
