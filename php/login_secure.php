baea3c1b739a1dcb385d465f7aa7ba6f4eb0e3dc
Timothe Medico <medico.timothe@gmail.com>
2018-05-13 19:57:40 +0200

    update login system

--- php/login_secure.php
+++ php/login_secure.php
@@ -15,9 +15,6 @@
 
         echo "Database connected <br>";
 
-        $number = 8/0;
-        phpinfo(;
-
         try {
             $reply = $bdd->query("SELECT COUNT(*) FROM admin WHERE username='$username' AND password='$password';");
         } catch (Exception $e){
